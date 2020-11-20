2ad9dbf594467a156b2d88e3e949dde0a6fbb375
Minny Park <opensource@thenx.org>
2020-11-20 15:48:04 +0800

    fixed: test module

---
+++ remote-test/src/main/java/org/thenx/RemoteTestApplication.java
@@ -0,0 +1,13 @@
+package org.thenx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteTestApplication.class, args);
+	}
+
+}
